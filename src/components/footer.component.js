2004036adfd6e49d018972fd15666cd402f9b4f8
Tiphaine <tiphaine.francoise@gmail.com>
2019-08-31 16:44:24 +0200

    init mern app

---
+++ src/components/footer.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <p>React Footer Component!</p>
+            </div>
+        );
+    }
+}
---
+++ src/components/header.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <p>React Header Component!</p>
+            </div>
+        );
+    }
+}
